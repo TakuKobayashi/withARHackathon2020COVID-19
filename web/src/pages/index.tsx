ed087851381c13af117619ef6d43d9456b9831d9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-04-18 16:24:17 +0900

    canvas表示してWebCame起動しようかと言うところまで

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,21 +1,50 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import * as handTrack from 'handtrackjs';
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
 
-const IndexPage = () => (
-  <IndexLayout>
-    <Page>
-      <Container>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Container>
-    </Page>
-  </IndexLayout>
-)
+class IndexPage extends React.Component {
+  constructor(props: any) {
+    super(props);
+    handTrack.load().then(model => {
+      // detect objects in the image.
+      console.log("model loaded");
+      console.log(model);
+    });
+  }
+
+  onCanvasLoaded = (canvas: HTMLCanvasElement) => {
+    if (!canvas) {
+      return;
+    }
+  }
+
+  render():
+  | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+  | string
+  | number
+  | {}
+  | React.ReactNodeArray
+  | React.ReactPortal
+  | boolean
+  | null
+  | undefined {
+  return (
+      <IndexLayout>
+        <Page>
+          <Container>
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <canvas ref={this.onCanvasLoaded} />
+          </Container>
+        </Page>
+      </IndexLayout>
+    )
+  }
+}
 
 export default IndexPage
