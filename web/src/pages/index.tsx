a01427730f595ece2f8fca1df07af30b951fd323
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-04-19 10:53:04 +0900

    refactor コードを整理しました

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,97 +1,91 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-import * as handTrack from 'handtrackjs';
-import * as faceapi from 'face-api.js';
-import Webcam from "react-webcam";
+import * as handTrack from 'handtrackjs'
+import * as faceapi from 'face-api.js'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
 
 class IndexPage extends React.Component {
-  private webcam: Webcam;
-  private handTrackModel: handTrack.ObjectDetection | null = null;
-  private isFaceTrackModelLoaded: boolean = false;
-  private cameraVideo: HTMLVideoElement | null;
-  private canvas: HTMLCanvasElement;
-  private context: any;
-  private isStartTracking = false;
+  private handTrackModel: handTrack.ObjectDetection | undefined = undefined
+
+  private isFaceTrackModelLoaded = false
+
+  private cameraVideo: HTMLVideoElement | null = null
+
+  private canvas: HTMLCanvasElement | null = null
+
+  private context: CanvasRenderingContext2D | null = null
+
+  private isStartTracking: boolean = false
 
   constructor(props: any) {
-    super(props);
-    console.log(faceapi.nets);
+    super(props)
+    console.log(faceapi.nets)
     faceapi.nets.tinyFaceDetector.loadFromUri('https://justadudewhohacks.github.io/face-api.js/models').then(() => {
-      console.log("faceLoaded");
-      this.isFaceTrackModelLoaded = true;
-      this.runDetection();
-    });
+      console.log('faceLoaded')
+      this.isFaceTrackModelLoaded = true
+      this.runDetection()
+    })
     handTrack.load().then(handModel => {
-      console.log(handModel);
-      this.handTrackModel = handModel;
-      this.runDetection();
-    });
-    this.runDetection = this.runDetection.bind(this);
+      console.log(handModel)
+      this.handTrackModel = handModel
+      this.runDetection()
+    })
+    this.runDetection = this.runDetection.bind(this)
   }
 
-  componentDidMount () {
-    //console.log(this.webcam.video);
-    //console.log(faceapi.createCanvasFromMedia(this.webcam.video));
-  }
-
-  onCanvasLoaded = (canvasRef: HTMLCanvasElement) => {
-    console.log(canvasRef);
+  onCanvasLoaded(canvasRef: HTMLCanvasElement) {
+    console.log(canvasRef)
     if (!canvasRef) {
-      return;
+      return
     }
-    this.canvas = canvasRef;
-    this.context = canvasRef.getContext("2d");
+    this.canvas = canvasRef
+    this.context = canvasRef.getContext('2d')
   }
 
-  onWebcamRef = (webcamRef: Webcam) => {
-    this.webcam = webcamRef;
-  }
-
-  onVideoRef = (videoRef) => {
+  onVideoRef(videoRef: HTMLVideoElement) {
     handTrack.startVideo(videoRef).then((isSuccess: boolean) => {
-      this.cameraVideo = videoRef;
-      if(isSuccess){
-        this.runDetection();
+      this.cameraVideo = videoRef
+      if (isSuccess) {
+        this.runDetection()
       }
-    });
+    })
   }
 
   runDetection() {
-    if(!this.cameraVideo){
-      return;
+    if (!this.cameraVideo) {
+      return
     }
-    if(this.handTrackModel){
+    if (this.handTrackModel) {
       this.handTrackModel.detect(this.cameraVideo).then(predictions => {
-        this.isStartTracking = true;
-        console.log("Predictions: ", predictions);
-        //this.handTrackModel.renderPredictions(predictions, this.canvas, this.context, this.cameraVideo);
-        window.requestAnimationFrame(this.runDetection);
-      });
+        this.isStartTracking = true
+        console.log('Predictions: ', predictions)
+        // this.handTrackModel.renderPredictions(predictions, this.canvas, this.context, this.cameraVideo);
+        window.requestAnimationFrame(this.runDetection)
+      })
     }
-    if(this.isFaceTrackModelLoaded){
+    if (this.isFaceTrackModelLoaded) {
       faceapi.detectSingleFace(this.cameraVideo, new faceapi.TinyFaceDetectorOptions()).then(faces => {
-        console.log("faces: ", faces);
-        window.requestAnimationFrame(this.runDetection);
-      });
+        console.log('faces: ', faces)
+        window.requestAnimationFrame(this.runDetection)
+      })
     }
   }
 
   render():
-  | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-  | string
-  | number
-  | {}
-  | React.ReactNodeArray
-  | React.ReactPortal
-  | boolean
-  | null
-  | undefined {
-  return (
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | string
+    | number
+    | {}
+    | React.ReactNodeArray
+    | React.ReactPortal
+    | boolean
+    | null
+    | undefined {
+    return (
       <IndexLayout>
         <Page>
           <Container>
